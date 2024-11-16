42b15c5ea18de61bb179ca1b81266b972d1e934a
Marcus <jm.turboz@gmail.com>
2024-11-16 16:45:54 -0300

    loc 2

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Dados de exemplo dos pontos de coleta
+// Dados dos pontos de coleta
 const pontosColeta = [
-    { tipo: "plastic", coords: [-14.8420111,-40.9250037], descricao: "Aterro sanitario- Rod. BA 262, km 08" },
-    { tipo: "eletronic", coords: [-14.8705438,-40.8307048], descricao: "Ecoponto - Rua São Luis, Candeias" },
-    { tipo: "eletronic", coords: [-14.8630441,-40.8493186], descricao: "Ecoponto- Av. Panamá, Bairro Jurema" }
+    { tipo: "plastic", coords: [-14.8420111, -40.9250037], descricao: "Aterro sanitario- Rod. BA 262, km 08" },
+        { tipo: "eletronic", coords:[-14.8711074,-40.8300021], descricao: "Ecoponto - Rua São Luis, Candeias" },
+    { tipo: "eletronic", coords: [-14.8630441, -40.8493186], descricao: "Ecoponto- Av. Panamá, Bairro Jurema" },
+    { tipo: "eletronic", coords: [-14.859801, -40.8355971], descricao: "Sacramentinas" },
+    { tipo: "eletronic", coords: [-14.8633135, -40.8295843], descricao: "Nova escola" },
+    { tipo: "eletronic", coords: [-14.8553948, -40.8304541], descricao: "Maria Salome" },
+    { tipo: "eletronic", coords: [-14.8537179, -40.8241393], descricao: "Uniftc" },
+    { tipo: "eletronic", coords: [-14.8418607, -40.8797098], descricao: "IFBA" },
+    { tipo: "eletronic", coords: [-14.9124939, -40.8781083], descricao: "PRF" },
+    { tipo: "eletronic", coords: [-14.861752, -40.8481126], descricao: "DISEP" },
+    { tipo: "eletronic", coords: [-14.8548981, -40.8487257], descricao: "Tecnoagil" },
+    { tipo: "eletronic", coords: [-14.8717784, -40.8534293], descricao: "Toyota Diamantina" },
+    { tipo: "eletronic", coords: [-14.8713889, -40.8739362], descricao: "Movel" },
+    { tipo: "eletronic", coords: [-14.8716667, -40.8546134], descricao: "Cambui" },
+    { tipo: "eletronic", coords: [-14.8747877, -40.8544676], descricao: "Topvel" },
+    { tipo: "eletronic", coords: [-14.8548654, -40.8495761], descricao: "Hospital São Vicente" },
+    { tipo: "eletronic", coords: [-14.8547964, -40.830635], descricao: "Vittasaude" },
+    { tipo: "eletronic", coords: [-14.8590064, -40.8349847], descricao: "ICON" }
 ];
 
 // Função para adicionar os pontos de coleta no mapa
